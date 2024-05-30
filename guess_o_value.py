f7b2d0360075021c332903957b42bc855b2498fe
Gabs <112837004+Gabs051@users.noreply.github.com>
2024-05-30 13:12:30 -0300

    Update guess_o_value.py
    05/30/2024
    added comments;
    added a progress bar in place of the seconds counter;

--- guess_o_value.py
+++ guess_o_value.py
@@ -1,10 +1,13 @@
-import random
-import time
+#importing libraries that we will use
+from random import randint
+from time import sleep
+from tqdm import tqdm
 
 print(f'\033[1;36mWelcome to the game Guess o Value!!!\033[m')
 user = str(input('\033[1;32mEnter your name: \033[m'))
 
 level = int(input('\033[1;36mSelect difficulty:\n\033[1;32m(1)Easy\n\033[1;31m(2)Hard\n\033[1;36mDifficulty: \033[m'))
+#user will enter a value according to the difficulty they want and will go through a check
 while level <= 0 or level > 2:
     print('\n\033[1;33mThere was an error choosing a value, enter again...')
     level = int(input('\033[1;36mSelect difficulty:\n\033[1;32m(1)Easy\n\033[1;31m(2)Hard\n\033[1;36mDifficulty: \033[m'))
@@ -12,11 +15,12 @@ while level <= 0 or level > 2:
 value_min = int(input('\033[1;36mEnter a minimum value: \033[m'))
 value_max = int(input('\033[1;36mEnter a maximum value: \033[m'))
 counter = 0
-machine_value = random.randint(value_min, value_max)
+machine_value = randint(value_min, value_max)
 
 while True:
     user_value = int(input(f'\033[1;36mEnter a value between {value_min} to {value_max}: \033[m'))
 
+    #checking if the value is within the range that the user wanted
     while user_value < value_min or user_value > value_max:
         print('\n\033[1;33mThere was an error choosing a value, enter again...\033[m')
         user_value = int(input(f'\033[1;36mEnter a value between {value_min} to {value_max}: \033[m'))
@@ -37,7 +41,8 @@ while True:
             print(f'\n\033[1;32mCongratulations you guessed the value, {user}!!!, wait a few seconds to see your score...\033[m')
             break
 
-for i in range(3, 0, -1):
-    print(f'\033[1;33m{i} Seconds...\033[m')
-    time.sleep(1)
-print(f'\033[1;36mHere this your scores:\nAttempts: \033[1;32m{counter}\033[m')
+#progress bar
+for i in tqdm(range(10), desc="\033[1;36mCalculating"):
+    sleep(0.3)
+
+print(f'\n\033[1;36mHere this your scores:\nAttempts: \033[1;32m{counter}\033[m')
